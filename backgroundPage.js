7dc4d11016b2ffe48a412cbc5d7c7e016efb470d
Ying Fu <saharaying@gmail.com>
2015-01-03 20:55:52 +0800

    support other type of notifications

--- backgroundPage.js
+++ backgroundPage.js
@@ -2,16 +2,18 @@
 
     'use strict';
 
-
     var active = false;
     var frequency = 60 * 1000;
     var timeoutHandle;
     var currentTimestamp;
 
-    var GD_URL_KEY = 'gd-new-entry-url';
-
     var GD_HOST = 'https://jinshuju.net/';
     var GD_NOTIFICATION_URL = GD_HOST + 'notifications/summary';
+
+    var icons = {
+      newEntry: 'images/new_entry.png',
+      default: 'images/message.png'
+    };
 
     function showNotification(icon, title, body, url) {
         var options = {
@@ -27,20 +29,37 @@
 
     }
 
+    function notificationOptions (content) {
+        if (content.indexOf('新增了') > -1) {
+            return {title: '新数据', icon: icons.newEntry, linkIndex: 1};
+        }
+
+        if (content.indexOf('向你分享了表单') > -1) {
+            return {title: '分享表单', icon: icons.default};
+        }
+
+        if (content.indexOf('数据推送失败') > -1) {
+            return {title: '数据推送失败', icon: icons.default};
+        }
+
+        if (content.indexOf('您提交的模板') > -1) {
+            return {title: '模板审核', icon: icons.default};
+        }
+        return {title: '新消息', icon: icons.default};
+    }
+
     function handleResponse(response) {
         var tmpContainer = document.createElement("div");
         tmpContainer.innerHTML = response;
         var tmpFragment = document.createDocumentFragment();
         while (i = tmpContainer.firstChild) tmpFragment.appendChild(i);
-        window.tmpFragment = tmpFragment;
         var list = tmpFragment.firstChild.children;
+
         for(var i = 0; i < list.length; i+= 1) {
-            var content = list[i].textContent;
-            if (content.indexOf('新增') > -1) {
-                var elements = list[i].children;
-                var entriesUrl = GD_HOST + elements[1].getAttribute('href');
-                showNotification('images/new_entry.png', '新数据', content, entriesUrl);
-            }
+            var content = list[i].textContent.trim();
+            var options = notificationOptions(content);
+            var url = GD_HOST + list[i].children[options.linkIndex || 0].getAttribute('href');
+            showNotification(options.icon, options.title, content, url);
         }
     }
 
@@ -57,18 +76,13 @@
     }
 
     function requestGDNewEntry () {
-//        chrome.storage.sync.get(GD_URL_KEY, function(items) {
-//            sendRequest(items[GD_URL_KEY]);
-//        });
         sendRequest(GD_NOTIFICATION_URL);
     }
-
 
     function start () {
         currentTimestamp = Math.round((new Date().getTime()) / 1000) - (12 * 3600);
         requestGDNewEntry();
     }
-
 
     function stop () {
         clearTimeout(timeoutHandle);
